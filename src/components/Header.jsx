ad39c58766a9c4f4274f07a30584f67ee026da3b
Anthony <anthony.fedorenko@gmail.com>
2021-07-26 19:32:00 +0300

    Commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+
+
+
+const Header = () => {
+    return <header className='header'>
+        <img src='https://cdn.logo.com/hotlink-ok/logo-social.png' />
+    </header>
+}
+
+export default Header
---
+++ src/components/Profile.jsx
@@ -0,0 +1,26 @@
+
+
+const Profile = () => {
+    return <div className='content'>
+        <div>
+            <img src="https://eskipaper.com/images/beach-seascape-1.jpg" alt=""/>
+        </div>
+        <div>
+            ava + description
+        </div>
+        <div>
+            my posts
+            <div>
+                new post
+            </div>
+            <div>
+                post1
+            </div>
+            <div>
+                post1
+            </div>
+        </div>
+    </div>
+}
+
+export default Profile
