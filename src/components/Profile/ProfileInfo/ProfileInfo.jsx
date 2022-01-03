513648a8ca70e307344406ff64cb36aaf10eb16c
Anthony <anthony.fedorenko@gmail.com>
2022-01-03 17:56:00 +0200

    authReducer, profileReducer refactored async/await usersReducer refactored follow/unfollow async/await, refactored cases (added objectHelpers). Login.jsx refactored Fields User.jsx, Paginator.jsx added Users.jsx, UsersContainer.jsx, ProfileInfo.jsx refactored

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -3,20 +3,15 @@ import s from './ProfileInfo.module.css'
 import Preloader from "../../Common/Preloader/Preloader";
 import ProfileStatusWithHooks from "./ProfileStatusWithHooks";
 
-const ProfileInfo = (props) => {
-    if (!props.profile) {
+const ProfileInfo = ({profile, status, updateStatus}) => {
+    if (!profile) {
         return <Preloader />
     }
     return (
         <div>
-            {/*<div>*/}
-            {/*    <img*/}
-            {/*        src="https://highload.today/wp-content/uploads/2021/07/204718748_2961989494123364_6520009051359628154_n-889x500.jpg"*/}
-            {/*        alt=""/>*/}
-            {/*</div>*/}
             <div className={s.descriptionBlock}>
-                <img src={props.profile.photos.large}/>
-                <ProfileStatusWithHooks status={props.status} updateStatus={props.updateStatus}/>
+                <img src={profile.photos.large}/>
+                <ProfileStatusWithHooks status={status} updateStatus={updateStatus}/>
             </div>
         </div>
     )
--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import reportWebVitals from './reportWebVitals';
 import store from "./Redux/reduxStore";
 import ReactDOM from 'react-dom';
 import './index.css';
@@ -8,18 +7,11 @@ import {BrowserRouter} from "react-router-dom";
 import {Provider} from "react-redux";
 
     ReactDOM.render(
-        // <React.StrictMode>
             <BrowserRouter>
                 <Provider store={store}>
                     <App/>
                 </Provider>
             </BrowserRouter>,
-        // </React.StrictMode>,
         document.getElementById('root')
     );
 
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
